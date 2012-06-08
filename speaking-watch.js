bd33bbe34976a1650cb603dc22c6744c05d13009
Thomas Duplomb <duplom_t@epitech.eu>
2012-06-09 01:50:16 +0200

    fonction de sortie de la date au format texte

--- speaking-watch.js
+++ speaking-watch.js
@@ -1,9 +1,41 @@
 $(function() {
 	console.log("coucou je suis le script");
+	init();
 	update();
 });
 
+function speaked_time(date) {
+	function round_minutes(minutes) {
+		return ~~(minutes <= 30 ? (minutes % 30) / 5 : (30 - (minutes % 30)) / 5);
+	}
+
+	var minutesText = ["", "FIVE", "TEN", "QUARTER", "TWENTY", "TWENTY FIVE", "HALF"];
+	var hoursText = ["TWELVE", "ONE", "TWO", "THREE", "FOUR", "FIVE",
+					"SIX", "SEVEN", "EIGHT", "NINE", "TEN", "ELEVEN"];
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var text = "IT IS ";
+
+	text += minutesText[~~(minutes <= 30 ? (minutes % 30) / 5 : (30 - (minutes % 30)) / 5)];
+	if (round_minutes(minutes) != 0)
+	{
+		if (minutes <= 30)
+			text += " PAST ";
+		else
+			text += " TO ";
+	}
+	text += hoursText[(hours  + (minutes <= 30 ? 0 : 1)) % 12];
+	if (round_minutes(minutes) == 0)
+		 text += " OCLOCK";
+	console.log(text);
+}
+
+function init() {
+
+}
+
 function update() {
 	console.log("update !");
+	speaked_time(new Date());
     window.setTimeout(update, 1000);
 }

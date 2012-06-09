d68b60a46c18bba9e335cfe5a16ad3477630313c
Thomas Duplomb <duplom_t@epitech.eu>
2012-06-09 12:17:30 +0200

    Temps qui s'affiche correctement, affichage des minutes sur des dots

--- speaking-watch.js
+++ speaking-watch.js
@@ -5,7 +5,12 @@ $(function() {
 
 function speakedTime(date) {
 	function roundMinutes(minutes) {
-		return ~~(minutes <= 30 ? (minutes % 30) / 5 : (30 - (minutes % 30)) / 5);
+		var ret = ~~((minutes % 60) / 5);
+		if (minutes > 30)
+			ret = 12 - ret;
+		console.log(ret);
+		return ret;
+		//return ~~(minutes <= 30 ? : ((30 - (minutes % 30)) / 5) + 1);
 	}
 
 	var minutesText = ["", "FIVE", "TEN", "QUARTER", "TWENTY", "TWENTY FIVE", "HALF"];
@@ -18,14 +23,21 @@ function speakedTime(date) {
 	text += minutesText[roundMinutes(minutes)];
 	if (roundMinutes(minutes) != 0)
 	{
-		if (minutes <= 30)
+		if (minutes < 35)
 			text += " PAST ";
 		else
 			text += " TO ";
 	}
 	text += hoursText[(hours  + (minutes <= 30 ? 0 : 1)) % 12];
 	if (roundMinutes(minutes) == 0)
-		text += " OCLOCK";
+		text += " OCLOCK ";
+	for (var i = minutes; i >= 0; i--) {
+		minutes[i]
+	};
+	for (var i = minutes % 5; i > 0; i--) {
+		text += " . ";
+	};
+	console.log(text);
 	return text;
 }
 
@@ -34,11 +46,11 @@ function init() {
 }
 
 function update() {	
-	var node = "ITLISASTIME<br />ACQUARTERDC<br />TWENTYFIVEX<br />HALFBTENFTO<br />PASTERUNINE<br />ONESIXTHREE<br />FOURFIVETWO<br />EIGHTELEVEN<br />SEVENTWELVE<br />TENSEOCLOCK<br />";
+	var node = "ITLISASTIME<br />ACQUARTERDC<br />TWENTYFIVEX<br />HALFBTENFTO<br />PASTERUNINE<br />ONESIXTHREE<br />FOURFIVETWO<br />EIGHTELEVEN<br />SEVENTWELVE<br />TENSEOCLOCK<br />.....";
 
 	node = highlightNode(node, speakedTime(new Date()));
 	$("#watch").html(node);
-    window.setTimeout(update, 1000);
+    window.setTimeout(update, 500);
 };
 
 function highlightNode(node, text) {
@@ -50,8 +62,7 @@ function highlightNode(node, text) {
 	{
 		node = node.replace(splited[i], "<span class='highlight'>" + splited[i] + "</span>");
 		tmp += node.slice(0, node.indexOf(splited[i]) + splited[i].length + 7);
-		console.log("node = " + node);
-		node = node.slice(node.indexOf(splited[i]) + splited[i].length + 7, -1);
+		node = node.slice(node.indexOf(splited[i]) + splited[i].length + 7);
 	}
 	tmp += node.slice(0, -1);
 	return tmp;
